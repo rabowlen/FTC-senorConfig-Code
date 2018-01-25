aa9cad65eb57d2803666fe429f5ca6ab6f0422ee
rabowlen <rabowlen@gmail.com>
2018-01-25 10:09:33 -0600

    Update AutoSenor
    Updated the code to make the color sensor an object that is used in AutoSenor.java rather than its own opmode.

--- AutoSenor.java
+++ AutoSenor.java
@@ -1,15 +1,12 @@
 /*
 Copyright 2017 FIRST Tech Challenge Team 12978
-
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
 associated documentation files (the "Software"), to deal in the Software without restriction,
 including without limitation the rights to use, copy, modify, merge, publish, distribute,
 sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
-
 The above copyright notice and this permission notice shall be included in all copies or substantial
 portions of the Software.
-
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
 NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
 NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
@@ -50,72 +47,72 @@ public class AutoSenor extends LinearOpMode {
     private Servo leftClaw;
     private Servo rightArm;
     private Servo leftArm;
-    
 
-    
+
+
     //Methods to lift and lower the claw arm.
     public void liftArm(){
-                rightArm.setPosition(1);
-                leftArm.setPosition(0);
-            } 
-            
+        rightArm.setPosition(1);
+        leftArm.setPosition(0);
+    }
+
     public void lowerArm(){
-                rightArm.setPosition(0);
-                leftArm.setPosition(1);
-            } 
-    
-    //Methods to automatically open or close the claw.        
+        rightArm.setPosition(0);
+        leftArm.setPosition(1);
+    }
+
+    //Methods to automatically open or close the claw.
     public void grab(){
         rightClaw.setPosition(.75);
         leftClaw.setPosition(.15);
     }
-    
+
     public void release(){
         rightClaw.setPosition(.15);
         leftClaw.setPosition(.75);
     }
-            
-    //A method to stop the wheels.        
+
+    //A method to stop the wheels.
     public void brake(){
         frontLeft.setPower(0);
         frontRight.setPower(0);
-        backLeft.setPower(0);  
+        backLeft.setPower(0);
         backRight.setPower(0);
     }
-    
+
     //Methods to move the bot true forward or backward.
     public void driveForward(){
         frontLeft.setPower(-0.5);
         frontRight.setPower(0.5);
-        backLeft.setPower(-0.5);  
+        backLeft.setPower(-0.5);
         backRight.setPower(0.5);
-        }
-        
+    }
+
     public void driveBackward(){
         frontLeft.setPower(0.5);
         frontRight.setPower(-0.5);
         backLeft.setPower(0.5);
         backRight.setPower(-0.5);
-        }
-        
-        //Methods to rotate the bot on 0 radius.
-        public void rotateRight(){
-            frontLeft.setPower(-0.5);
-            frontRight.setPower(-0.5);
-            backLeft.setPower(-0.5);
-            backRight.setPower(-0.5);
-        }
-        
-        public void rotateLeft(){
-            frontLeft.setPower(0.5);
-            frontRight.setPower(0.5);
-            backLeft.setPower(0.5);
-            backRight.setPower(0.5);
-        }
-        
-        
-        
-    //Declare a variable for time.    
+    }
+
+    //Methods to rotate the bot on 0 radius.
+    public void rotateRight(){
+        frontLeft.setPower(-0.5);
+        frontRight.setPower(-0.5);
+        backLeft.setPower(-0.5);
+        backRight.setPower(-0.5);
+    }
+
+    public void rotateLeft(){
+        frontLeft.setPower(0.5);
+        frontRight.setPower(0.5);
+        backLeft.setPower(0.5);
+        backRight.setPower(0.5);
+    }
+
+
+
+    //Declare a variable for time.
     ElapsedTime eTime = new ElapsedTime();
 
     @Override
@@ -129,33 +126,38 @@ public class AutoSenor extends LinearOpMode {
         rightArm = hardwareMap.get(Servo.class, "rightArm");
         leftArm = hardwareMap.get(Servo.class, "leftArm");
 
+        SensorREVColorDistance color_sensor = new SensorREVColorDisance();
+
         telemetry.addData("Status", "Initialized");
         telemetry.update();
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
 
         // run until the end of the match (driver presses STOP)
-            telemetry.addData("Status", "Running");
-            telemetry.update();
-            
-            //Grab onto the preset block
-            grab();
-            liftArm();
-            
-            eTime.reset();
-            while(eTime.time() < .6)
-                driveForward();
-            while(eTime.time() < 1.2)
-                rotateLeft();
-            while(eTime.time() < 2.2)
-                driveForward();
-                lowerArm();
-            
-            
-            
-            
+        telemetry.addData("Status", "Running");
+        telemetry.update();
 
-        }
-        
-        
+        //Trigger the start of the color sensor.
+        color_sensor.runOpMode();
+
+        //Grab onto the preset block
+        grab();
+        liftArm();
+
+        eTime.reset();
+        while(eTime.time() < .6)
+            driveForward();
+        while(eTime.time() < 1.2)
+            rotateLeft();
+        while(eTime.time() < 2.2)
+            driveForward();
+        lowerArm();
+
+
+
+
+
     }
+
+
+}
